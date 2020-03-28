2ae2d97a0749eb6f632ac751f1dfe6db9e7dd934
Satyam-Mishra-7410 <mishrasatyam28@gmail.com>
2020-03-28 16:56:55 +0530

    Added base project

---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Transform center, obj1, obj2, resultVector;
+    public Vector3 dir1 { get { return obj1.position - center.position; } }
+    public Vector3 dir2 { get { return obj2.position - center.position; } }
+    [Range(0,360)]
+    public float angle = 0;
+
+    public void Update()
+    {
+        resultVector.position = Utility.GetDir(dir1, center.position, angle);
+    }
+}
---
+++ Assets/Utility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public static class Utility
+{
+	public static float GetClockAngle(this Vector3 dir1, Vector3 dir2)
+	{
+		float dot = dir1.x * dir2.x + dir1.y * dir2.y;          //# dot product between [x1, y1] and [x2, y2]
+		float det = dir1.x * dir2.y - dir1.y * dir2.x;          //# determinant
+		float angle = Mathf.Atan2(det, dot) * Mathf.Rad2Deg;    //# atan2(y, x) or atan2(sin, cos)
+
+		return (angle >= 0) ? angle : 360 - Mathf.Abs(angle);
+	}
+
+	/// <summary>
+	/// Returns a vector with a rotation of angle degree in anticlockwise direction
+	/// </summary>
+	/// <param name="dir1"></param>
+	/// <param name="center"></param>
+	/// <param name="angle"> for random angle just pass a random angle</param>
+	/// <returns></returns>
+	public static Vector3 GetDir(this Vector3 dir1, Vector3 center, float angle)
+	{
+		float tempAngle = (angle + Vector3.right.GetClockAngle(dir1)) * Mathf.Deg2Rad; // in radians
+		Vector3 resultDir = new Vector3(Mathf.Cos(tempAngle), Mathf.Sin(tempAngle), 0) + center;
+		return resultDir;
+	}
+
+	public static float DotProduct(this Vector3 dir1, Vector3 dir2)
+	{
+		return (dir1.x * dir2.x + dir1.y * dir2.y + dir1.z * dir2.z);
+	}
+
+	public static Vector3 CrossProduct(this Vector3 dir1, Vector3 dir2)
+	{
+		return new Vector3(((dir1.y * dir2.z) - (dir1.z * dir2.y)), ((dir1.z * dir2.x) - (dir1.x * dir2.z)), ((dir1.x * dir2.y) - (dir1.y * dir2.x)));
+	}
+}
